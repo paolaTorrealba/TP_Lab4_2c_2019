b300c142bcf10834001ed5ef7e998e75e5ac1e58
Paola <torrealbapa@hotmail.com.ar>
2019-11-14 02:36:49 -0300

    tengo sueño

--- comanda/src/app/componentes/cerrar-mesa/cerrar-mesa.component.ts
+++ comanda/src/app/componentes/cerrar-mesa/cerrar-mesa.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { AuthProvider } from 'src/app/providers/auth';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable, empty } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cerrar-mesa',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cerrar-mesa.component.scss']
 })
 export class CerrarMesaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public mesas:Array<any> = [];
+  public estado:string="cerrada";
+  constructor(private  data:  AuthService,   
+    private auth: AuthProvider) { 
+    this.obtenerMesas();
   }
+
+  ngOnInit() {}
+
+  obtenerMesas(){
+    this.data.getListaPedidos("mesas").subscribe(lista => {
+      this.mesas=lista; 
+      console.log("mesas: ",this.mesas); 
+    });
+    console.log("mesas: ",this.mesas)
+   }
+
+   cerrarMesa(item){
+      console.log("item: ", item)        
+      item.estado=this.estado;    
+      this.auth.updateMesa(item).then(res => {
+        console.log("mesa cerrada")
+      });
+   }
 
 }
