2da683b942996271905c5521454766a31977a9f4
Paola <torrealbapa@hotmail.com.ar>
2019-11-24 05:30:12 -0300

    agrego directiva

--- comanda/src/app/componentes/listado-pedidos/listado-pedidos.component.ts
+++ comanda/src/app/componentes/listado-pedidos/listado-pedidos.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { AuthProvider } from 'src/app/providers/auth';
 import { AngularFireStorage } from '@angular/fire/storage';
-
+import { MatTableDataSource, MatDialog } from '@angular/material';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-listado-pedidos',
@@ -11,13 +12,21 @@ import { AngularFireStorage } from '@angular/fire/storage';
 })
 export class ListadoPedidosComponent implements OnInit {
 
+  public info:boolean;
+  private columsPedido: string[] = ['Mesa', 'Importe', 'Estado', 'Tiempo Elaboracion','Detalle'];
+  private columsProductoPedido: string[] = ['Descripcion'];
   public pedidos:Array<any> = [];
-
+  public productos:Array<any> = [];
+  private dataSourceProd : any;
+  private noDataProd: any;
+  private dataSource = new MatTableDataSource(this.pedidos);
+  private noData = this.dataSource.connect().pipe(map((data: any[]) => data.length === 0));
 
   constructor(private  data:  AuthService,
     private storage: AngularFireStorage, 
     private auth: AuthProvider) { 
     this.obtenerPedidos();
+    this.info=false;
   }
 
   ngOnInit() {
@@ -31,4 +40,18 @@ export class ListadoPedidosComponent implements OnInit {
     console.log("pedidos: ",this.pedidos)
    }
 
+   showInfo(item){
+     if (!this.info){
+        console.log("item", item)
+        this.info=true;
+        this.productos=item.productos;
+        console.log("productos", this.productos)      
+     }else{
+        this.info=false;
+        this.productos = [];     
+     }
+     this.dataSourceProd = new MatTableDataSource(this.productos);
+      this.noDataProd = this.dataSource.connect().pipe(map((data: any[]) => data.length === 0));
+    
+   }
 }
