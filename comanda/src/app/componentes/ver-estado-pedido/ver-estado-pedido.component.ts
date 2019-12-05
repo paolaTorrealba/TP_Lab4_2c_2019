9d3171db280b5c47f2ff07c04f15068750ec54ba
Paola <torrealbapa@hotmail.com.ar>
2019-12-05 06:26:41 -0300

    ver pedido

--- comanda/src/app/componentes/ver-estado-pedido/ver-estado-pedido.component.ts
+++ comanda/src/app/componentes/ver-estado-pedido/ver-estado-pedido.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { AuthProvider } from 'src/app/providers/auth';
-import { EstadoPedido } from 'src/app/clases/enum';
+import { EstadoPedido, Perfil } from 'src/app/clases/enum';
+import { Observable, empty } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { MatTableDataSource } from '@angular/material';
+import { map } from 'rxjs/operators';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
+import { DataApiService } from 'src/app/servicios/data-api.service';
+import { take } from 'rxjs/operators';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 
 @Component({
@@ -10,27 +18,83 @@ import { EstadoPedido } from 'src/app/clases/enum';
   styleUrls: ['./ver-estado-pedido.component.scss']
 })
 export class VerEstadoPedidoComponent implements OnInit {
-  public pedidos:Array<any> = [];
+ 
   public recibido:string="recibido";
   public entregado:EstadoPedido.entregado;  
   public correo:string;
-  
+  public nombre:string;
+  public perfil:Perfil;
+  public vacia:boolean;
+  public info:boolean;
 
+  private columsPedido: string[] = ['Nombre Cliente', 'Codigo Mesa', 'Estado','Detalle','Codigo Pedido','Monto Total','Confirmar Recepcion'];
+  private columsProductoPedido: string[] = ['Descripcion','Precio','Estado Producto','Tiempo Promedio Elaboracion'];
+
+  public pedidos:Array<any> = [];
+  public misPedidos:Array<any> = [];
+  public productos:Array<any> = [];
+
+  private dataSourceProd : any;
+  private noDataProd: any;
+  private dataSource = new MatTableDataSource(this.misPedidos);
+  private noData = this.dataSource.connect().pipe(map((data: any[]) => data.length === 0));
   constructor(private  data:  AuthService,    
-    private auth: AuthProvider) { 
-      this.correo=localStorage.getItem("usuarioComanda");
-      this.obtenerPedidos();
+    private auth: AuthProvider,
+    private router: Router,
+    private usuarioService: UsuarioService,
+    private dataApi: DataApiService) {    
+      this.obtenerUsuario();
     }
 
   ngOnInit() {}
 
+
+  obtenerUsuario() { 
+    this.usuarioService.EstaLogeado().subscribe(user => {
+      if (user) {       
+        this.dataApi.TraerUno(user.uid, 'usuarios')
+        .pipe(take(1)).subscribe(userx => {
+          if (userx) {
+            if (userx.activo) {             
+                this.usuarioService.usuario = userx;             
+                this.correo= userx.correo;
+                this.nombre = userx.nombre;            
+                this.perfil = userx.perfil;    
+                this.obtenerPedidos(); 
+            }
+            else {             
+              this.nombre = "";
+              this.correo= ""; 
+              this.perfil = null;
+            }
+          }
+        });
+      }
+      else {
+        this.nombre = "";
+        this.correo= "";       
+        this.perfil = null;
+      }
+    });
+  }
   obtenerPedidos(){
     this.data.getListaPedidos("pedidos").subscribe(lista => {
       this.pedidos=lista; 
-      console.log("pedidos: ",this.pedidos); 
+      console.log("1: ",this.pedidos)
+      console.log("2 ",this.pedidos.length)
+      for (let i=0; i<=this.pedidos.length-1; i++){
+        if(this.pedidos[i].correoCliente==this.correo
+          && this.pedidos[i].estado!=EstadoPedido.cancelado){
+            
+          this.misPedidos.push( this.pedidos[i])
+          this.vacia=false;
+          this.dataSource = new MatTableDataSource(this.misPedidos); 
+               
+        }
+      }      
     });
-    console.log("pedidos: ",this.pedidos)
-   }
+   
+  }
 
    
    confirmarRecepcion(item){    
@@ -42,4 +106,19 @@ export class VerEstadoPedidoComponent implements OnInit {
       console.log("pedido recibido por el cliente")
     });
  }
+
+ showInfo(item){
+  if (!this.info){
+     console.log("item", item)
+     this.info=true;
+     this.productos=item.productos;
+     console.log("productos", this.productos)      
+  }else{
+     this.info=false;
+     this.productos = [];     
+  }
+  this.dataSourceProd = new MatTableDataSource(this.productos);
+   this.noDataProd = this.dataSourceProd.connect().pipe(map((data: any[]) => data.length === 0));
+ 
+}
 }
