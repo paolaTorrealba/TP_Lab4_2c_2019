979c847b26c65edd9412ffef40df0c938ba031d9
Paola <torrealbapa@hotmail.com.ar>
2019-12-12 14:28:29 -0300

    actualizo repo

--- comanda/src/app/componentes/card-reportes/card-reportes.component.ts
+++ comanda/src/app/componentes/card-reportes/card-reportes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-card-reportes',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-reportes.component.scss']
 })
 export class CardReportesComponent implements OnInit {
+  @ViewChild("imgLogin", { static: false })
+  InputImagenLogin: ElementRef;
 
-  constructor() { }
+  constructor() { 
+   
+  }
 
   ngOnInit() {
+  
+    // document.getElementById('socio-img').attributes.getNamedItem;
   }
 
 }
